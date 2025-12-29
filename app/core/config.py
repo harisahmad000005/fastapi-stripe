ff637e53318f901bb66e2668655513f2e822bf58
Haris Ahmad <harisahmad000005@gmail.com>
2025-12-29 14:16:44 +0500

    formated the code

--- app/core/config.py
+++ app/core/config.py
@@ -2,23 +2,19 @@ from pydantic import BaseSettings, AnyUrl
 
 
 class Settings(BaseSettings):
-APP_ENV: str = "development"
-SECRET_KEY: str
-HOST: str = "0.0.0.0"
-PORT: int = 8000
+    APP_ENV: str = "development"
+    SECRET_KEY: str
+    HOST: str = "0.0.0.0"
+    PORT: int = 8000
 
+    DATABASE_URL: AnyUrl
 
-DATABASE_URL: AnyUrl
+    STRIPE_SECRET_KEY: str
+    STRIPE_WEBHOOK_SECRET: str
+    STRIPE_SUCCESS_URL: str
+    STRIPE_CANCEL_URL: str
 
-
-STRIPE_SECRET_KEY: str
-STRIPE_WEBHOOK_SECRET: str
-STRIPE_SUCCESS_URL: str
-STRIPE_CANCEL_URL: str
-
-
-class Config:
-env_file = ".env"
-
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
